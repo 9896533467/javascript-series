3b987653110128d2671b827848ed413d05162e46
9896533467 <118165562+9896533467@users.noreply.github.com>
2024-05-06 09:30:05 +0000

    Var let const datatype

--- js-code/test.js
+++ js-code/test.js
@@ -1,1 +1,35 @@
-console.log("Vishal");
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// assingned in memory like accountId is the memory name
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+after variable declare we not assigned any value to variable so it show undefined
+let accountState; shows undefined
+Q- null and undefined difference
+Q-const let var difference
+Q-why do we do not use var nowdays
+Q-what if we not assign variable to name
+*/
+
+/*
+Datatypes-
+number 
+bigint
+string =""
+boolean = true/false
+null = standalone value
+undefined 
+symbol = unique
+
+object
+console.log(typeof age) //number
+console.log(typeof undefined) undefined
+*/
